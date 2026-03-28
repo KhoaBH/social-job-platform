49fb8c5a2d9765d096c307720c552415288665a8
KhoaBH <lamanhkhoa2004@gmail.com>
2026-03-28 14:28:18 +0700

    feat: health check api

---
+++ src/main/java/vn/edu/uit/socialjob/platform/common/controller/HealthController.java
@@ -0,0 +1,18 @@
+package vn.edu.uit.socialjob.platform.common.controller;
+
+import java.time.Instant;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping
+    public String check() {
+        return "OK";
+    }
+}
